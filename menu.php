893ebf15c2eb01aca5386438ac574ee4c622f564
Gustavo Eda <edapd12@gmail.com>
2015-07-18 23:25:58 -0300

    alteraçoes mapa

--- menu.php
+++ menu.php
@@ -2,14 +2,18 @@
   $menuDisciplinas = "";
   $menuRequisitos  = "";
   $menuProfessores = "";
+  $mapa = "";
 
   if (isset($menuSelected) && $menuSelected == 3) {
     $menuProfessores = "selected";
   } else if (isset($menuSelected) && $menuSelected == 2) {
     $menuRequisitos = "selected";
-  } else {
+  } else if (isset($menuSelected) && $menuSelected == 4){
+    $mapa = "selected";
+  }else{
     $menuDisciplinas = "selected";
   }
+
 ?>
 <div id="menu">
   <h1>BCCS</h1>
@@ -22,6 +26,9 @@
     </a>
     <a href="professores.php">
       <li class="<?= $menuProfessores ?>">Professores</li>
+    </a>
+    <a href="mapa.php">
+      <li class="<?= $mapa ?>">Mapa</li>
     </a>
     <!-- <li>Atividades Desenvolvidas</li> -->
     <a href="http://dcomp.sorocaba.ufscar.br/wp-content/docs/projetoPedagogicoBCCS-2010.pdf" target="_blank">
