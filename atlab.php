ce4590a5ecb4f3f3a7da45897d49d3ef7b5c806a
Gustavo Eda <edapd12@gmail.com>
2015-07-21 22:09:06 -0300

    atualização do mapa e criação da pagina de detalhamento do atlab

---
+++ atlab.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include "header.php"; ?>
+</head>
+<body>
+  <?php $menuSelected = 4; include "menu.php" ?>
+  <div id="content">
+    <header>ATLAB - Edificio de Aulas Teoricas e Laboratorios</header>
+    <div id="main-content">
+    <h1>Térreo</h1>
+    <img src="images\ATLab-Terreo.jpg" height="450" width="1050">
+    <h2>1º Andar</h2>
+    <img src="images\ATLab-1Andar.jpg" height="450" width="1050">
+    </div>
+  </div>
+</body>
+</html>
